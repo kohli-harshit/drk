2f5c45beb521432748dfdc53999bf08e21907a5e
kohli-harshit <harshit.kohli@monotype.com>
2017-05-20 12:49:18 +0530

    refactoring code

--- excel_operations.js
+++ excel_operations.js
@@ -1,9 +1,7 @@
 var XLSX = require('xlsx');
 var basefile = 'UserInfo.xlsx';
 
-exports.getMachineInfo = getMachineInfo;
-
-var getMachineInfo = function (machineName) {
+getMachineInfo = function (machineName) {
     //Load Excel File
     var workbook = XLSX.readFile(basefile);
     var first_sheet_name = workbook.SheetNames[0];
@@ -43,3 +41,65 @@ var getMachineInfo = function (machineName) {
     }
     return result;
 };
+
+addMachine = function(machineName) {
+
+    console.log("Add Machine called for Machine - " + machineName);
+    return "(Mock) Machine Added successfully";
+    /*
+    var workbook = XLSX.readFile(basefile);
+    var first_sheet_name = workbook.SheetNames[0];
+    var worksheet = workbook.Sheets[first_sheet_name];
+    var range = XLSX.utils.decode_range(worksheet['!ref']); 
+    for(var R = range.s.r; R <= range.e.r; ++R) {
+        for(var C = range.s.c; C <= range.e.c; ++C) {
+                //find the cell object
+            var cellref = XLSX.utils.encode_cell({c:C, r:R}); // construct A1 reference for cell
+                if(!worksheet[cellref]) continue; // if cell doesn't exist, move on
+                    var cell = worksheet[cellref];
+
+                // if the cell is a text cell with the old string, change it
+                    if(cell.v === "Blank") 
+                    {  
+                                cell.v=completemachinenameinput;
+                                XLSX.writeFile(workbook, './excelConverter/obj.xlsx');
+                                bot.reply(message, 'Got it. The machine '+completemachinenameinput+ ' is added successfully ');
+                                R=range.e.r;
+                                C=range.s.c;
+                                convo.next();
+
+                    }
+                                                                                                
+        }
+    }
+    */
+}
+
+isMachinePresent = function(machineName){
+    //Code to check wheather machine is already in dossier or not
+    var workbook = XLSX.readFile(basefile);
+    var first_sheet_name = workbook.SheetNames[0];
+    var worksheet = workbook.Sheets[first_sheet_name];
+    var range = XLSX.utils.decode_range(worksheet['!ref']); 
+    var flag=false;
+
+    //Find the Machine
+    for(var R = range.s.r; R <= range.e.r; ++R) {
+        for(var C = range.s.c; C <= range.e.c; ++C) {
+
+            /* find the cell object */
+            var cellref = XLSX.utils.encode_cell({c:C, r:R}); // construct A1 reference for cell
+            if(!worksheet[cellref]) continue; // if cell doesn't exist, move on
+            var cell = worksheet[cellref];
+
+            /* if the cell is a text cell with the old string, change it */
+
+            if(cell.v === machineName) {                
+                flag=true;
+                break;
+            }
+        }
+    }
+
+    return flag;
+}
--- slack_bot.js
+++ slack_bot.js
@@ -5,19 +5,21 @@ if (!process.env.token) {
 }
 
 //Initialize Required Packages and Start the Bot
-var Botkit = require('./lib/Botkit.js');
+var Botkit = require('./lib/Botkit.js'),
+    mongoStorage = require('botkit-storage-mongo')({mongoUri: 'mongodb://noi-qa-jenkins:27017/drk-db'}),
+    controller = Botkit.slackbot({
+        storage: mongoStorage
+    });
 var Excel = require('./excel_operations.js');
 var os = require('os');
 var fs = require('fs');
-var controller = Botkit.slackbot({
-    debug: true,
-});
+var ping = require('ping');
 var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
 
 //Reply to Hello and Hey
-controller.hears(['hello', 'hey','hi'], 'direct_message,direct_mention,mention', function (bot, message) {
+controller.hears(['hello', 'hey'], 'direct_message,direct_mention,mention', function (bot, message) {
 
     bot.api.reactions.add({
         timestamp: message.ts,
@@ -28,7 +30,6 @@ controller.hears(['hello', 'hey','hi'], 'direct_message,direct_mention,mention',
             bot.botkit.log('Failed to add emoji reaction :(', err);
         }
     });
-
 
     controller.storage.users.get(message.user, function (err, user) {
         if (user && user.name) {
@@ -49,7 +50,7 @@ controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_men
             };
         }
         user.name = name;
-        controller.storage.users.save(user, function (err, id) {
+        controller.storage.users.save(user, function (err, id) {            
             bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
         });
     });
@@ -134,269 +135,72 @@ controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your na
 
 //User wants a machine
 controller.hears(['who is using (.*)','what is the status of (.*)','i want to use (.*)'],'direct_message,direct_mention,mention',function(bot,message) {
-    var excelEntry=Excel.getMachineInfo(message.match[1]);
-    bot.reply(message,excelEntry);
+    var machineAssignee=getMachineInfo(message.match[1]);
+    bot.reply(message,machineAssignee);
 });
 
-controller.hears(['add new machine (.*)', 'add a new machine (.*)'], 'direct_message,direct_mention', function (bot, message) {
-    var completemachinenameinput = message.match[1];
-    var machinenamechararray = completemachinenameinput.split('');
-    var first_sheet_name = workbook.SheetNames[0];
-    var worksheet = workbook.Sheets[first_sheet_name];
-    lengthofinput = completemachinenameinput.length - 1;
-    var machinename = " ";
-    var machinetype = " ";
-    var machinelocation = " ";
-    //to calculate machinename
-    var numberofdash = 0;
-    var lengthofinput1 = lengthofinput;
-    //for number of dash calculation
-    while (lengthofinput1 > 0) {
-        if (machinenamechararray[lengthofinput1] == '-') {
-            numberofdash++;
-        }
-        lengthofinput1--;
-    }
-    if (numberofdash == 2) {
-        while ((machinenamechararray[lengthofinput] != '-')) //||(machinenamechararray[lengthofinput]!=" "))
-        {
-            machinename = machinename + completemachinenameinput[lengthofinput]
-            lengthofinput--;
-
-        }
-        if (lengthofinput > 0) {
-            lengthofinput--;
-            // bot.reply(message, machinename);
-            //to calculate machine type
-            while ((machinenamechararray[lengthofinput] != '-')) //||(machinenamechararray[lengthofinput]!=" "))
+controller.hears(['add new machine (.*)','add a new machine (.*)'],'direct_message,direct_mention,mention',function(bot,message) {
+     var machineNameEntered = message.match[1];
+     
+     bot.startConversation(message, function(err, convo) {
+       convo.ask('Are you sure you want to add '+machineNameEntered + ' ?', [
             {
-                machinetype = machinetype + completemachinenameinput[lengthofinput];
-                lengthofinput--;
-            }
-        }
-        if (lengthofinput > 0) {
-            lengthofinput--;
-            //bot.reply(message, machinename+machinetype+lengthofinput+);
-            //to calculate machine location
-            while (lengthofinput >= 0) {
-                machinelocation = machinelocation + completemachinenameinput[lengthofinput];
-                lengthofinput--;
-            }
-        }
-        var machinename = reverse(machinename);
-        var machinetype = reverse(machinetype);
-        var machinelocation = reverse(machinelocation);
-        // bot.reply(message, 'machinename:-' +machinename +'machinetype:-' +machinelocation+ 'machinetype:-' +machinetype);
-        //length of machine name should be of length 2 
-        if ((machinename.length) == 2) {
-            machinename = "0" + machinename;
-        }
-        var machinetypechararray = machinetype.split('');
-        var lengthofmachinetype = machinetype.length;
-        var c = machinetypechararray[0];
-        var newmachinetype = " ";
-        if (c >= '0' && c <= '9') {
-            newmachinetype = newmachinetype + "one";
-            for (var i = 1; i < lengthofmachinetype; ++i) { // it is a number
-                newmachinetype = newmachinetype + machinetypechararray[i];
-            }
-        } else {
-            newmachinetype = " " + machinetype;
-        }
-        if (machinelocation.length == 1) {
-            machinelocation = " noi";
-        }
-        //removing spaces globally
-        machinelocation = machinelocation.replace(/\s/g, "");
-        machinename = machinename.replace(/\s/g, "");
-        newmachinetype = newmachinetype.replace(/\s/g, "");
-        //bot.reply(message,machinelocation+'-'+newmachinetype+'-'+machinename);
+                pattern: bot.utterances.yes,
+                callback: function(response, convo) {
+                    //If machine is not available in the dossier then ping the machine and add it
+                    if(!isMachinePresent(machineNameEntered)) {
 
-        // creating complete machine name
-        var completeoneboxname = machinelocation + '-' + newmachinetype + '-' + machinename;
-        //bot.reply(message,completeoneboxname);
-        var range = XLSX.utils.decode_range(worksheet['!ref']);
-        var flag = 0; // get the range
-        for (var R = range.s.r; R <= range.e.r; ++R) {
-            for (var C = range.s.c; C <= range.e.c; ++C) {
-                /* find the cell object */
-                var cellref = XLSX.utils.encode_cell({
-                    c: C,
-                    r: R
-                }); // construct A1 reference for cell
-                if (!worksheet[cellref]) continue; // if cell doesn't exist, move on
-                var cell = worksheet[cellref];
+                        var isMachineReachable = ping.sys.probe(host, function(isAlive){
+                            return isAlive;
+                        });
 
-                /* if the cell is a text cell with the old string, change it */
-                if (cell.v === completeoneboxname) {
-                    flag = 1;
-                }
-            }
-        }
-        //bot.reply(message,'flag is'+flag)
-        if (flag == 0) {
-            var range = XLSX.utils.decode_range(worksheet['!ref']);
-            for (var R = range.s.r; R <= range.e.r; ++R) {
-                for (var C = range.s.c; C <= range.e.c; ++C) {
-                    /* find the cell object */
-                    var cellref = XLSX.utils.encode_cell({
-                        c: C,
-                        r: R
-                    }); // construct A1 reference for cell
-                    if (!worksheet[cellref]) continue; // if cell doesn't exist, move on
-                    var cell = worksheet[cellref];
-                    //bot.reply(message,'this is '+R+' '+C);
-                    /* if the cell is a text cell with the old string, change it */
-                    if (cell.v === "Blank") { //bot.reply(message,'this is '+R+' '+C);
-                        cell.v = completeoneboxname;
-                        XLSX.writeFile(workbook, './excelConverter/obj.xlsx');
-                        bot.reply(message, 'Got it. The machine ' + completeoneboxname + ' is added successfully ');
-                        R = range.e.r;
-                        C = range.s.c;
-                        /* var cell2 = { v: "NA" };
-                                                                var cellref2 = XLSX.utils.encode_cell({c:C+1, r:R}); // construct A1 reference for cell
-                                                                cell2.t = 's'
-                                                                 // if cell doesn't exist, move on
-                                                                worksheet[cellref2]=cell2;
-                                                                worksheet['!ref'] = XLSX.utils.encode_range(range);
-                                                                workbook.SheetNames.push(first_sheet_name);s
-                                                                workbook.Sheets[first_sheet_name] = worksheet;
-                                                                XLSX.writeFile(workbook, './excelConverter/obj.xlsx');
-                                                                bot.reply(message, 'Got it. The machine'+completeoneboxname+ ' is added successfully ');
-
-                                                                //var cellref3 = XLSX.utils.encode_cell({c:C, r:R+1}); // construct A1 reference for cell
-                                                                //if(!worksheet[cellref3]) continue; // if cell doesn't exist, move on
-                                                                //var cell3 = worksheet[cellref3];
-                                                                //cell3.v="NEW";
-                                                                //XLSX.writeFile(workbook, './excelConverter/obj.xlsx');
-                                                                //sbot.reply(message, 'Got it. The machine'+completeoneboxname+ ' is added successfully ');
-                                                            */
+                        if(isMachineReachable)
+                        {                             
+                            convo.ask('Please Enter Administrator password to continue....', function(response, convo) {
+                                if(response.text=='Monotype456#') {
+                                    bot.reply(message,addMachine(machineNameEntered));
+                                }
+                                else {
+                                    bot.reply(message, 'Password is incorrect! Please retry to add a new machine again !');                                
+                                }
+                            });
+                        }
+                        else
+                        {
+                            bot.reply(message, 'The machine '+machineNameEntered+ ' is dead! Please check that you can ping the machine and then try again');
+                        }
                     }
+                    else
+                    {
+                        bot.reply(message,'This machine is already in my dossier please try with different machine');
+                    }
+                    convo.next();
+                }
+            },
+            {
+                pattern: bot.utterances.no,
+                default: true,
+                callback: function(response, convo) {                    
+                    bot.api.reactions.add({
+                        timestamp: message.ts,
+                        channel: message.channel,
+                        name: 'rage',
+                    }, function (err, res) {
+                        if (err) {
+                            bot.botkit.log('Failed to add emoji reaction :(', err);
+                        }
+                    });
+                    convo.next();
                 }
             }
-
-        }
-        if (flag == 1) {
-            bot.reply(message, ' This machine is already in my dossier please try with different machine');
-        }
-    } else {
-        bot.reply(message, ' Please provide machine name in correct format i.e. machinelocation-machinetype-machinename');
-    }
+        ]);
+    });
+    
 });
+
 
 controller.hears(['remove machine (.*)', 'remove a machine (.*)'], 'direct_message,direct_mention', function (bot, message) {
-    var completemachinenameinput = message.match[1];
-    var machinenamechararray = completemachinenameinput.split('');
-    var first_sheet_name = workbook.SheetNames[0];
-    var worksheet = workbook.Sheets[first_sheet_name];
-    lengthofinput = completemachinenameinput.length - 1;
-    var machinename = " ";
-    var machinetype = " ";
-    var machinelocation = " ";
-    //to calculate machinename
-    var numberofdash = 0;
-    var lengthofinput1 = lengthofinput;
-    //for number of dash calculation
-    while (lengthofinput1 > 0) {
-        if (machinenamechararray[lengthofinput1] == '-') {
-            numberofdash++;
-        }
-        lengthofinput1--;
-    }
-    if (numberofdash == 2) {
-        while ((machinenamechararray[lengthofinput] != '-')) //||(machinenamechararray[lengthofinput]!=" "))
-        {
-            machinename = machinename + completemachinenameinput[lengthofinput]
-            lengthofinput--;
-
-        }
-        if (lengthofinput > 0) {
-            lengthofinput--;
-            // bot.reply(message, machinename);
-            //to calculate machine type
-            while ((machinenamechararray[lengthofinput] != '-')) //||(machinenamechararray[lengthofinput]!=" "))
-            {
-                machinetype = machinetype + completemachinenameinput[lengthofinput];
-                lengthofinput--;
-            }
-        }
-        if (lengthofinput > 0) {
-            lengthofinput--;
-            //bot.reply(message, machinename+machinetype+lengthofinput+);
-            //to calculate machine location
-            while (lengthofinput >= 0) {
-                machinelocation = machinelocation + completemachinenameinput[lengthofinput];
-                lengthofinput--;
-            }
-        }
-        var machinename = reverse(machinename);
-        var machinetype = reverse(machinetype);
-        var machinelocation = reverse(machinelocation);
-        // bot.reply(message, 'machinename:-' +machinename +'machinetype:-' +machinelocation+ 'machinetype:-' +machinetype);
-        //length of machine name should be of length 2 
-        if ((machinename.length) == 2) {
-            machinename = "0" + machinename;
-        }
-        var machinetypechararray = machinetype.split('');
-        var lengthofmachinetype = machinetype.length;
-        var c = machinetypechararray[0];
-        var newmachinetype = " ";
-        if (c >= '0' && c <= '9') {
-            newmachinetype = newmachinetype + "one";
-            for (var i = 1; i < lengthofmachinetype; ++i) { // it is a number
-                newmachinetype = newmachinetype + machinetypechararray[i];
-            }
-        } else {
-            newmachinetype = " " + machinetype;
-        }
-        if (machinelocation.length == 1) {
-            machinelocation = " noi";
-        }
-        //removing spaces globally
-        machinelocation = machinelocation.replace(/\s/g, "");
-        machinename = machinename.replace(/\s/g, "");
-        newmachinetype = newmachinetype.replace(/\s/g, "");
-        //bot.reply(message,machinelocation+'-'+newmachinetype+'-'+machinename);
-
-        // creating complete machine name
-        var completeoneboxname = machinelocation + '-' + newmachinetype + '-' + machinename;
-        //bot.reply(message,completeoneboxname);
-        var range = XLSX.utils.decode_range(worksheet['!ref']);
-        var flag = 0; // get the range
-        for (var R = range.s.r; R <= range.e.r; ++R) {
-            for (var C = range.s.c; C <= range.e.c; ++C) {
-                /* find the cell object */
-                var cellref = XLSX.utils.encode_cell({
-                    c: C,
-                    r: R
-                }); // construct A1 reference for cell
-                if (!worksheet[cellref]) continue; // if cell doesn't exist, move on
-                var cell = worksheet[cellref];
-
-                /* if the cell is a text cell with the old string, change it */
-                if (cell.v === completeoneboxname) {
-                    flag = 1;
-                    cell.v = "Deleted";
-                    var cellref3 = XLSX.utils.encode_cell({
-                        c: C + 1,
-                        r: R
-                    }); // construct A1 reference for cell
-                    if (!worksheet[cellref3]) continue; // if cell doesn't exist, move on
-                    var cell3 = worksheet[cellref3];
-                    cell3.v = "Deleted";
-                    XLSX.writeFile(workbook, './excelConverter/obj.xlsx');
-                    bot.reply(message, 'Got it. The machine' + completeoneboxname + ' is deleted successfully ');
-
-                }
-            }
-        }
-        //bot.reply(message,'flag is'+flag)
-        if (flag == 0) {
-            bot.reply(message, ' This machine is not in my dossier please try with different machine'); //sbot.reply(message, 'Got it. The machine'+completeoneboxname+ ' is added successfully ');
-        }
-    } else {
-        bot.reply(message, ' Please provide machine name in correct format i.e. machinelocation-machinetype-machinename');
-    }
+    
 });
 
 function formatUptime(uptime) {
