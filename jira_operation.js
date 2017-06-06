6b6b613ee33e99d4700c9e2095b87f810b051ca3
nitinmonotype <nitin.mittal@monotype.com>
2017-06-06 11:25:12 +0530

    Revert "Revert "Testrail functioanlity done working code""
    This reverts commit 081a3a735be5a13620e50b42e8b20a14ba9e2854.

--- jira_operation.js
+++ jira_operation.js
@@ -1,12 +1,4 @@
 var Client = require('node-rest-client').Client;
-var async = require('async');
-var Q=require('q');
-var baseUrl="https://jira.monotype.com/rest/api/2/";
-getUrl=function(id)
-{
-   var jira_url=baseUrl + "issue/" + id; 
-   return jira_url;
-}
 // Provide user credentials, which will be used to log in to JIRA.
 var searchArgs = {
 
@@ -15,54 +7,17 @@ var searchArgs = {
                 "Authorization": "Basic c3NoYXJtYTpHb29nbGVAMTIzNA=="
                  } 
 };
-getSearchResultFormatted = function(searchResult)
+var searchUrl="https://jira.monotype.com/rest/api/2/issue/";
+getInformationById = function (taskId,callback)
 {
-        var Info=[];
-        Info.push({
-                 TaskId: searchResult.key,
-                 TaskType: searchResult.fields.issuetype.name,
-                 ParentId: searchResult.fields.parent.key,
-                 ProjectName:searchResult.fields.project.name,
-                 OriginalEstimates: searchResult.timetracking.originalEstimates,
-                 RemainingEstimates: searchResult.timetracking.remainingEstimates,
-                 Summary: searchResul.fields.summary,
-                 Creator:searchResult.fields.creator.displayName,
-                 Reporter:searchResult.fields.reporter.displayName,
-                 Assignee:searchResult.fields.assignee.displayName,
-                 Status:searchResult.fields.status.name
-               });
-                        console.log(Info);                     				
-                        return Id;
-}
-getInformationById = function (taskId,convo,message,bot,callback)
-{
- 
-client = new Client();            
-var getUrl_Promise=Q.denodeify(getUrl);
-var result=getUrl(taskId);                        
-result.then
- {   try{    
-   client.get(result, searchArgs, function(searchResult, response)
-       {        
+client = new Client();
+var finalUrlToSearch=searchUrl+"taskId";
+client.get(fianlUrlToSeach, Args, function(searchResult, response)
+       {
                 console.log(response.statusCode);
+                callback(searchResult);
                 console.log('search result:', searchResult);
-                if(response.statusCode=='200')
-                  {
-                        var getSearchResultFormatted_Promise=Q.denodeify(getSearchResultFormatted);
-                        var result=getSearchResultFormatted(searchResult);                        
-                        result.then
-                        {                                
-                        callback(result);}
-                  }
-                      else{
-                   bot.reply(message,'There is no issue with this taskid, Please recheck it');
-                   convo.stop();   
-                }    
+                
         });
 
-}catch(err)
-{
-console.log('error is' +err);
-}
- }
 }
